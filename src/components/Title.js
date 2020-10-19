2cff5f912ba19edbb83aae0f07e65cbb57990187
Alexis Ozaeta <enriqueozaetam@gmail.com>
2020-10-18 19:43:18 -0600

    [change] - Title component css class names using BEM.

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,7 @@ import  '../assets/styles/components/Title.scss'
 
 const title = () => {
     return (
-        <h4>Sorting Algorithm Visualizer</h4>
+        <h4 className="title">Sorting Algorithm Visualizer</h4>
     )
 }
 
