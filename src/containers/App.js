0d5c12669d50c64d399779c685e9b8b232a7b89a
Alexis Ozaeta <enriqueozaetam@gmail.com>
2020-11-02 17:47:57 -0700

    [change] - App is now class type. [new] - Methods in order to handle Header's components event.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Main from '../containers/Main'
 import '../assets/styles/containers/App.scss';
 
-const App = () => {
-	return (
-		<div className="App">
-			<Header /> 
-			<Main />
-			<Footer />
-		</div>
-	);
+class App extends Component {
+	state = {
+		heightMain: 0,
+		
+	}
+
+	handleCreateArray = (e) => {
+		//console.log(e.target);
+	}
+
+	handleSortArray = (e) => {
+		//console.log(e.target);
+	}
+
+	handleSelectAlgorithm = (e) => {
+		//console.log(e.target.value);
+	}
+
+	handleSlider = (e) => {
+		//console.log(e.target.value);
+	}
+
+	componentDidMount() {
+		const heightPage = document.getElementById("root").offsetHeight
+		const heightFooter = document.getElementsByClassName("footer")[0].offsetHeight
+		const heightHeader = document.getElementsByClassName("header")[0].offsetHeight
+		const heightMain = heightPage - heightHeader - heightFooter
+
+		this.setState({
+			...this.state,
+			heightMain: heightMain
+		})
+	}
+
+	render() {
+		const { heightMain } = this.state
+		
+		return (
+			<div className="App">
+				<Header 
+					onCreateArray={this.handleCreateArray} 
+					onSortArray={this.handleSortArray}
+					onSelectAlgorithm={this.handleSelectAlgorithm}
+					onChangeSlider={this.handleSlider} /> 
+				<Main 
+					heightMain={heightMain} />
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
